9bffa79a53d1e5abcb4482b245dcb1e2272622a4
Paul Hutchings <dev@studiop.page>
2024-04-09 21:32:48 -0400

    Switch nested loops to flatmap

--- ImmersiveMusicPatcher/Program.cs
+++ ImmersiveMusicPatcher/Program.cs
@@ -5,6 +5,7 @@ using Mutagen.Bethesda;
 using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
+using Noggog;
 
 namespace ImmersiveMusicPatcher
 {
@@ -35,20 +36,21 @@ namespace ImmersiveMusicPatcher
             var masterWorldspaces = immersiveMusicEsp.Mod.Worldspaces;
             // var ocwPatchEsp = "OCW_MusicPatch_IM.esp";
 
+            var interiorCellsToPatch = masterCells.Records
+                .SelectMany(cellBlock => cellBlock.SubBlocks)
+                .SelectMany(subBlock => subBlock.Cells);
+            interiorCellsToPatch.ForEach(cell => PatchCell(cell, state));
+
+            var worldspaceCellsToPatch = masterWorldspaces.Records
+                .SelectMany(worldspace => worldspace.SubCells)
+                .SelectMany(worldspaceBlock => worldspaceBlock.Items)
+                .SelectMany(worldspaceSubBlock => worldspaceSubBlock.Items);
+            worldspaceCellsToPatch.ForEach(cell => PatchCell(cell, state));
+        }
+
         private static void PatchCell(ICellGetter cell, IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
             // Check if cell music type matches one in immersive music otherwise deep copy and set in patcher
-            foreach (var masterCellBlockGetter in masterCells.Records) {
-                foreach (var masterCellSubBlock in masterCellBlockGetter.SubBlocks) {
-                    foreach (var immersiveMusicCell in masterCellSubBlock.Cells)
-                    {
-                        patchCell(immersiveMusicCell, state);
-                    }
-                }
-            }
-        }
-
-        private static void patchCell(ICellGetter cell, IPatcherState<ISkyrimMod, ISkyrimModGetter> state){
             if (!cell
                 .ToLink()
                 .TryResolveContext<ISkyrimMod, ISkyrimModGetter, ICell, ICellGetter>(
