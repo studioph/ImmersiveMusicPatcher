221c42af42ab1956140dab0144fead6ff72e5afa
Paul Hutchings <dev@studiop.page>
2024-04-09 21:31:41 -0400

    Format + fix

--- ImmersiveMusicPatcher/Program.cs
+++ ImmersiveMusicPatcher/Program.cs
@@ -11,14 +11,14 @@ namespace ImmersiveMusicPatcher
     public class Program
     {
         private static readonly ModKey ImmersiveMusic = ModKey.FromNameAndExtension("ImmersiveMusic.esp");
-        
+
         public static async Task<int> Main(string[] args)
         {
             return await SynthesisPipeline.Instance
                 .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
                 .SetTypicalOpen(GameRelease.SkyrimSE, "ImmersiveMusicPatcher.esp")
                 .AddRunnabilityCheck(state =>
-                { 
+                {
                     state.LoadOrder.AssertListsMod(ImmersiveMusic, "\n\nMissing ImmersiveMusic.esp\n\n");
                 })
                 .Run(args);
@@ -27,11 +27,16 @@ namespace ImmersiveMusicPatcher
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
             var immersiveMusicEsp = state.LoadOrder.GetIfEnabled(ImmersiveMusic);
-            if (immersiveMusicEsp.Mod == null) return;
+            if (immersiveMusicEsp.Mod == null)
+            {
+                return;
+            }
             var masterCells = immersiveMusicEsp.Mod.Cells;
             var masterWorldspaces = immersiveMusicEsp.Mod.Worldspaces;
             // var ocwPatchEsp = "OCW_MusicPatch_IM.esp";
 
+        private static void PatchCell(ICellGetter cell, IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
             // Check if cell music type matches one in immersive music otherwise deep copy and set in patcher
             foreach (var masterCellBlockGetter in masterCells.Records) {
                 foreach (var masterCellSubBlock in masterCellBlockGetter.SubBlocks) {
@@ -49,16 +54,17 @@ namespace ImmersiveMusicPatcher
                 .TryResolveContext<ISkyrimMod, ISkyrimModGetter, ICell, ICellGetter>(
                     state.LinkCache,
                     out var winningCellContext)
-            ) continue;
+            ) return;
 
             // Don't patch if music type matches existing
             if (cell.Music.Equals(winningCellContext.Record.Music))
             {
-                continue;
+                return;
             }
 
             var patchCell = winningCellContext.GetOrAddAsOverride(state.PatchMod);
-            if (!cell.Music.IsNull) {
+            if (!cell.Music.IsNull)
+            {
                 patchCell.Music.FormKey = cell.Music.FormKey;
             }
         }
